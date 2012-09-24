d1a6f4d8e80a6400864083d865a3347af24c59ac
Adam Rifat <adam@littledonkey.net>
2012-09-24 21:41:34 +0100

    Finally sussed calendar logic with a little jQuery help...

--- administrator/components/com_helloworld/js/tariffs.js
+++ administrator/components/com_helloworld/js/tariffs.js
@@ -3,31 +3,40 @@ $(document).ready(function(){
  
  
  $(function() {
+   
+   var start_date = $('#jform_tariffs_start_date_tariff_0').attr('value');
+   
+   if(start_date == '') {
+     start_date = new Date();
+   }
+   
 		$( "#jform_tariffs_start_date_tariff_0" ).datepicker({
-			numberOfMonths: 3,
-      minDate:-0,
+			numberOfMonths: 1,
       showOn:"both",
       dateFormat:"dd-mm-yy",
       buttonImageOnly:true,
      	buttonImage: "/media/system/images/calendar.png",
-
+      showButtonPanel: true,
 			onSelect: function( selectedDate ) {
 				$( "#jform_tariffs_end_date_tariff_0" ).datepicker( "option", "minDate", selectedDate );
-			}
+			},
+      minDate: start_date
 		});
     
 		$( "#jform_tariffs_end_date_tariff_0" ).datepicker({
-			numberOfMonths: 3,
-      minDate:-0,
+			numberOfMonths: 1,
       dateFormat:"dd-mm-yy",
       showOn:"both",
       buttonImageOnly:true,
      	buttonImage: "/media/system/images/calendar.png",
 			onSelect: function( selectedDate ) {
-				$( "#jform_tariffs_start_date_tariff_0" ).datepicker( "option", "maxDate", selectedDate );
-			}
+				// This is quite strict - disallows setting of a start date after the end date
+        //$( "#jform_tariffs_start_date_tariff_0" ).datepicker( "option", "maxDate", selectedDate );
+			},
+      minDate: start_date
+      
 		});
 	});
- 
+       
  
 })
