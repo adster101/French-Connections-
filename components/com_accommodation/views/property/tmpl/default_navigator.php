fdaf8f03514384d46fef010b0c2402727050a0a1
Adam Rifat <adam@littledonkey.net>
2012-11-09 17:11:17 +0000

    Added missing template to repo

---
+++ components/com_accommodation/views/property/tmpl/default_navigator.php
@@ -0,0 +1,9 @@
+<ul class="nav nav-tabs">
+  <li class="active">
+    <a href="#">Main</a>
+  </li>
+  <li><a href="#">Description</a></li>
+  <li><a href="#">Location</a></li>
+  <li><a href="#">Availability</a></li>
+  <li><a href="#">Pricing</a></li>
+</ul>
