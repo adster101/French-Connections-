da7934800868e03463f0bbb87d6d8931ad951a44
Adam Rifat <adam@littledonkey.net>
2012-12-04 17:40:46 +0000

    Added reviews, updated locality info and added unit tabs to property listing.

---
+++ components/com_accommodation/views/property/tmpl/default_units.php
@@ -0,0 +1,16 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+?>
+
+<ul class="nav nav-tabs">
+  <li><a href="#top">Top</a></li>
+  <li><a href="#description">Description</a></li>
+  <li><a href="#location">Locality</a></li>
+  <li><a href="#travel">Travel</a></li>
+  <li><a href="#activities">Activities</a></li>
+
+  <li><a href="#facilities">Facilities</a></li>
+  <li><a href="#availability">Availability</a></li>
+  <li><a href="#tariffs">Tariffs</a></li>
+</ul>
