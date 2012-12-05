302cd1ee83c504547e8302e3f2b6abc4d2f4a183
Adam Rifat <adam@littledonkey.net>
2012-12-05 09:33:59 +0000

    Update to unit tabs on property page.

--- components/com_accommodation/views/property/tmpl/default_units.php
+++ components/com_accommodation/views/property/tmpl/default_units.php
@@ -3,15 +3,22 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 
-
-
 <ul class="nav nav-tabs">
-  <?php foreach($this->units as $unit) : ?>
-  <li>
-    <a href="#">
-      <?php echo $unit->title; ?>
-    </a>
-  </li>
+  <?php foreach ($this->units as $unit) { ?>
+    <li <?php
+  if ($this->item->id == $unit->id) {
+    echo "class='active'";
+  }
+  ?>>
+      <a href="<?php echo JRoute::_('index.php?option=com_accommodation&view=property&id=' . $unit->id) ?>">
+  <?php echo $unit->title; ?><br />
+          <?php if ($this->item->occupancy) : ?>
 
-  <?php endforeach; ?>
+          <small><?php echo JText::_('COM_ACCOMMODATION_SITE_OCCUPANCY'); ?>
+    <?php echo $unit->occupancy; ?></small>
+    <?php endif; ?>
+      </a>
+    </li>
+<?php } ?>
 </ul>
+
