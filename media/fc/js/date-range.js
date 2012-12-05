b4eef7b18adf71d75a009062ce89e54902f4ffb4
Adam Rifat <adam@littledonkey.net>
2012-12-05 09:38:01 +0000

    Adding missing JS and reviews files.

---
+++ media/fc/js/date-range.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function(){
+ 
+ 
+ jQuery(function() {
+   
+
+    var start_date = jQuery('.start_date').attr('value');
+
+    if(start_date == '') {
+      start_date = new Date();
+    }
+   
+		jQuery('.start_date').datepicker({
+			numberOfMonths: 1,
+      showOn:"both",
+      dateFormat:"dd-mm-yy",
+      buttonImageOnly:true,
+     	buttonImage: "/media/system/images/calendar.png",
+      showButtonPanel: true,
+			onSelect: function( selectedDate ) {
+				jQuery('.end_date').datepicker( "option", "minDate", selectedDate );
+			},
+      minDate: new Date()
+		});
+    
+		jQuery('.end_date').datepicker({
+			numberOfMonths: 1,
+      dateFormat:"dd-mm-yy",
+      showOn:"both",
+      buttonImageOnly:true,
+     	buttonImage: "/media/system/images/calendar.png",
+      minDate: start_date,
+      showButtonPanel: true
+		});
+     
+	});
+})
