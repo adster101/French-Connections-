9243a75a26e19196392f7afcc5274885d35f63a4
Adam Rifat <adam@littledonkey.net>
2013-05-28 13:02:05 +0100

    Commit stats update ready for some testing...

--- administrator/components/com_helloworld/models/stats.php
+++ administrator/components/com_helloworld/models/stats.php
@@ -9,6 +9,7 @@ jimport('joomla.application.component.modellegacy');
 // Import the graph gubbins
 jimport('pchart.class.pDraw');
 jimport('pchart.class.pImage');
+jimport('pchart.class.pData');
 
 /**
  * HelloWorld Model
@@ -22,34 +23,90 @@ class HelloWorldModelStats extends JModelLegacy {
   protected $id = '';
 
   public function getGraph() {
-    // Get a pData instance
-    $graph = new pDraw();
 
+    // Get a pData instance
+    $graph_data = new pData;
+
+    /* Create the pChart object */
+    $myPicture = new pImage(700, 230, $graph_data);
+
+    // Get the id from the model state
     $id = $this->getState('stats.id');
 
     // Get the property views and what not
     $data = $this->getData($id, '#__property_views');
 
-    foreach ($data as $key => $value) {
-      /* Save the data in the pData array */
-      $graph->addPoints($value->year, "Year");
-      $graph->addPoints($value->month, "Month");
-      $graph->addPoints($value - count, "Count");
+    // Gets an array of the last X months in reverse order
+    for ($x = 0; $x < 12; $x++) {
+      $months[] = date('mY', mktime(0, 0, 0, date('m') - $x, 1));
     }
 
-    $myData->setAbscissa("Month");
+    // Sort and then reverse the array
+    ksort($months, SORT_DESC);
+    $months = array_reverse($months);
 
-    /* Associate the "Humidity" data serie to the second axis */
-    $myData->setSerieOnAxis("Count", 1);
+    // Set up an array to hold the series data
+    $count = array();
 
-    /* Name this axis "Time" */
-    $myData->setXAxisName("Count");
+    // Based on each month, we loop over the property data and if data exists for that month add it to the array
+    foreach ($months as $key => $value) {
 
-    /* Specify that this axis will display time values */
-    $myData->setXAxisDisplay(AXIS_FORMAT_TIME, "H:i");
+      if (array_key_exists($value, $data)) {
+        $count[] = $data[$value];
+      } else {
+        $count[] = 0;
+      }
+    }
 
 
-    return $data;
+    // Gets an array of the last X months in reverse order
+    for ($x = 0; $x < 12; $x++) {
+      $formatted_months[] = date('M', mktime(0, 0, 0, date('m') - $x, 1));
+    }
+
+    // Sort and then reverse the array
+    ksort($formatted_months, SORT_DESC);
+
+    $formatted_months = array_reverse($formatted_months);
+
+    $graph_data->addPoints($count, 'Page views');
+
+    $graph_data->setSerieOnAxis("Page views", 0);
+
+    $graph_data->setSerieWeight("Page views",1);
+
+    $graph_data->setAxisName(0, "Total");
+
+    /* Bind a data serie to the X axis */
+    $graph_data->addPoints($formatted_months, "Labels");
+    $graph_data->setSerieDescription("Labels", "Months");
+    $graph_data->setAbscissa("Labels");
+
+    /* Choose a nice font */
+    $myPicture->setFontProperties(array("FontName" => JPATH_SITE . "/libraries/pchart/fonts/verdana.ttf", "FontSize" => 11));
+
+    /* Define the boundaries of the graph area */
+    $myPicture->setGraphArea(60, 40, 670, 190);
+    $myPicture->drawText(0, 0, JTEXT::_('Property Statistics for ' . (int) $id), array("FontSize" => 20, "Align" => TEXT_ALIGN_TOPLEFT));
+
+    /* Draw the scale, keep everything automatic */
+    $myPicture->drawScale(array("GridR" => 180, "GridG" => 180, "GridB" => 180, "DrawSubTicks" => true));
+
+    /* Write a legend box */
+    $myPicture->setFontProperties(array("FontName" => JPATH_SITE . "/libraries/pchart/fonts/verdana.ttf", "FontSize" => 9, "R" => 80, "G" => 80, "B" => 80));
+    $myPicture->drawLegend(250, 50, array("Style" => LEGEND_BOX, "BoxSize" => 4, "R" => 200, "G" => 200, "B" => 200, "Surrounding" => 20, "Alpha" => 30));
+
+    /* Draw the scale, keep everything automatic */
+    $myPicture->drawLineChart();
+
+    $filename = "_stats" . (int) $id . '.png';
+
+    /* Render the picture */
+    $myPicture->Render(JPATH_SITE . "/cache/" . $filename);
+
+    echo '<img src = "' . JURI::root() . '/cache/' . $filename . '" />';
+
+    return $graph_data;
   }
 
   public function populateState() {
@@ -68,21 +125,27 @@ class HelloWorldModelStats extends JModelLegacy {
     $query = $db->getQuery(true);
 
     $query->select('
-       month(date_created) as month,
-       year(date_created) as year,
-       count(id) as count
+       count(id) as count,
+       concat(month(date_created), year(date_created)) as month
      ');
     $query->from($table);
     $query->where('property_id = ' . (int) $id);
     $query->where('date_created > ' . $db->quote(date('Y-m-d', $last_year)));
     $query->group('YEAR(date_created)');
     $query->group('month(date_created)');
-
+    $query->order('year(date_created) asc');
     $db->setQuery($query);
 
-    $rows = $db->loadObjectList();
+    $rows = $db->loadAssocList();
 
-    return $rows;
+    $data = array();
+
+    // Key the array on the months returned
+    foreach ($rows as $key => $value) {
+      $data['0' . $value['month']] = $value['count'];
+    }
+
+    return $data;
   }
 
 }
--- administrator/components/com_helloworld/views/stats/tmpl/default.php
+++ administrator/components/com_helloworld/views/stats/tmpl/default.php
@@ -3,5 +3,4 @@
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
-print_r($this->data);
 
