8c1d63bdfbd1cfe3cfd37d3420891938f1243f9e
Adam Rifat <adam@littledonkey.net>
2013-09-27 16:13:23 +0100

    Pesky file format

--- components/com_registerowner/views/registerowner/view.html.php
+++ components/com_registerowner/views/registerowner/view.html.php
@@ -1,47 +1,39 @@
-<?php    
+<?php
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
- 
- 
+
 /**
  * HTML View class for the HelloWorld Component
  */
-class RegisterOwnerViewRegisterowner extends JViewLegacy
-{
+class RegisterOwnerViewRegisterowner extends JViewLegacy {
+
   protected $state;
-	protected $form;
-	protected $item;
-	protected $return_page;
-  
-	// Overwriting JView display method
-	function display($tpl = null) 
-	{
-   
-    
+  protected $form;
+  protected $item;
+  protected $return_page;
+
+// Overwriting JView display method
+  function display($tpl = null) {
+
+
     $this->state = $this->get('State');
-    
+
     $this->form = $this->get('Form');
-      
-    
-		// Set the document
-		$this->setDocument();		
+
+
+// Set the document
+    $this->setDocument();
     parent::display($tpl);
+  }
 
-	}
-
-	/**
-	 * Method to set up the document properties
-	 *
-	 * @return void
-	 */
-	protected function setDocument() 
-	{
-		
-   
-
-
-  
+  /**
+   * Method to set up the document properties
+   *
+   * @return void
+   */
+  protected function setDocument() {
     
-	}	
+  }
+
 }
