c6a42dd05d9ea78c4f15e07c2e805eb9bc2120f9
Adam Rifat <adam@littledonkey.net>
2013-10-29 13:47:23 +0000

    Added thumbnail to featured property module.

--- modules/mod_featuredproperty/helper.php
+++ modules/mod_featuredproperty/helper.php
@@ -32,9 +32,9 @@ class modFeaturedPropertyHelper {
     $query->select('
       a.id,
       b.id as unit_id,
-      -- c.thumbnail,
       c.unit_title,
       c.occupancy,
+      i.image_file_name as thumbnail,
       g.title,
       (select min(tariff) from qitz3_tariffs i where i.unit_id = b.id and end_date > now() group by unit_id ) as price
     ');
@@ -43,12 +43,14 @@ class modFeaturedPropertyHelper {
     // Here we only join the unit version where review is 0. Should ensure that we only take published units
     $query->join('left', '#__unit_versions c on (b.id = c.unit_id and c.id = (select max(d.id) from qitz3_unit_versions d where unit_id = b.id and review = 0))');
     // Same goes for the property version, which we join to get the location
-    $query->join('left','#__property_versions e on (a.id = e.property_id and e.id = (select max(f.id) from qitz3_property_versions f where property_id = a.id and review = 0))' );
-    $query->join('left','#__classifications g ON g.id = e.department');
-    $query->join('left','#__featured_properties h on h.property_id = a.id');
+    $query->join('left', '#__property_versions e on (a.id = e.property_id and e.id = (select max(f.id) from qitz3_property_versions f where property_id = a.id and review = 0))' );
+    $query->join('left', '#__classifications g ON g.id = e.department');
+    $query->join('left', '#__featured_properties h on h.property_id = a.id');
+    $query->join('left', '#__property_images_library i on e.id = i.version_id');
     $query->where('b.ordering = 1');
     $query->where('a.published = 1');
     $query->where('b.published = 1');
+    $query->where('i.ordering = 1');
     $query->where('a.expiry_date >= ' . $db->quote(JFactory::getDate()->calendar('Y-m-d')));
     $query->where('h.published = 1');
     $query->where('h.featured_property_type = ' . $type);
