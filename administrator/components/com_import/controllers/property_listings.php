f98235bb08989f0e94fe47627db31528fed0daf8
Adam Rifat <adam@littledonkey.net>
2014-02-19 13:59:24 +0000

    Amended property versions import routine to convert to utf8

--- administrator/components/com_import/controllers/property_listings.php
+++ administrator/components/com_import/controllers/property_listings.php
@@ -13,12 +13,48 @@ jimport('joomla.user.helper');
  */
 class ImportControllerProperty_listings extends JControllerForm {
 
+  function cp1252_to_utf8($str) {
+    global $cp1252_map;
+    return strtr(utf8_encode($str), $cp1252_map);
+  }
+
   public function import() {
 
     // Check that this is a valid call from a logged in user.
     JSession::checkToken('POST') or die('Invalid Token');
 
     $config = JFactory::getConfig();
+    $cp1252_map = array(
+        "\xc2\x80" => "\xe2\x82\xac", /* EURO SIGN */
+        "\xc2\x82" => "\xe2\x80\x9a", /* SINGLE LOW-9 QUOTATION MARK */
+        "\xc2\x83" => "\xc6\x92", /* LATIN SMALL LETTER F WITH HOOK */
+        "\xc2\x84" => "\xe2\x80\x9e", /* DOUBLE LOW-9 QUOTATION MARK */
+        "\xc2\x85" => "\xe2\x80\xa6", /* HORIZONTAL ELLIPSIS */
+        "\xc2\x86" => "\xe2\x80\xa0", /* DAGGER */
+        "\xc2\x87" => "\xe2\x80\xa1", /* DOUBLE DAGGER */
+        "\xc2\x88" => "\xcb\x86", /* MODIFIER LETTER CIRCUMFLEX ACCENT */
+        "\xc2\x89" => "\xe2\x80\xb0", /* PER MILLE SIGN */
+        "\xc2\x8a" => "\xc5\xa0", /* LATIN CAPITAL LETTER S WITH CARON */
+        "\xc2\x8b" => "\xe2\x80\xb9", /* SINGLE LEFT-POINTING ANGLE QUOTATION */
+        "\xc2\x8c" => "\xc5\x92", /* LATIN CAPITAL LIGATURE OE */
+        "\xc2\x8e" => "\xc5\xbd", /* LATIN CAPITAL LETTER Z WITH CARON */
+        "\xc2\x91" => "\xe2\x80\x98", /* LEFT SINGLE QUOTATION MARK */
+        "\xc2\x92" => "\xe2\x80\x99", /* RIGHT SINGLE QUOTATION MARK */
+        "\xc2\x93" => "\xe2\x80\x9c", /* LEFT DOUBLE QUOTATION MARK */
+        "\xc2\x94" => "\xe2\x80\x9d", /* RIGHT DOUBLE QUOTATION MARK */
+        "\xc2\x95" => "\xe2\x80\xa2", /* BULLET */
+        "\xc2\x96" => "\xe2\x80\x93", /* EN DASH */
+        "\xc2\x97" => "\xe2\x80\x94", /* EM DASH */
+        "\xc2\x98" => "\xcb\x9c", /* SMALL TILDE */
+        "\xc2\x99" => "\xe2\x84\xa2", /* TRADE MARK SIGN */
+        "\xc2\x9a" => "\xc5\xa1", /* LATIN SMALL LETTER S WITH CARON */
+        "\xc2\x9b" => "\xe2\x80\xba", /* SINGLE RIGHT-POINTING ANGLE QUOTATION */
+        "\xc2\x9c" => "\xc5\x93", /* LATIN SMALL LIGATURE OE */
+        "\xc2\x9e" => "\xc5\xbe", /* LATIN SMALL LETTER Z WITH CARON */
+        "\xc2\x9f" => "\xc5\xb8" /* LATIN CAPITAL LETTER Y WITH DIAERESIS */
+    );
+
+
 
     // Create a log file for the email kickers
     jimport('joomla.error.log');
@@ -61,12 +97,11 @@ class ImportControllerProperty_listings extends JControllerForm {
     // Get the nearest city/town based on the lat and long
     $db = JFactory::getDbo();
     $db->truncateTable('#__property_versions');
-    
+
     while (($line = fgetcsv($handle, 0, $delimiter = '|')) !== FALSE) {
 
-
+      
       $query = $db->getQuery(true);
-
       $latitude = ($line[11]) ? $line[11] : '46.589069';
       $longitude = ($line[12]) ? $line[12] : '2.416992';
 
@@ -146,22 +181,23 @@ class ImportControllerProperty_listings extends JControllerForm {
           'phone_2', 'phone_3', 'fax', 'email_1', 'email_2', 'website', 'review', 'created_on', 'created_by',
           'modified_on', 'modified_by', 'published_on', 'languages_spoken'
       ));
-      $insert_string = '';
 
+      $insert_string = '';
       // May need revising
       $insert_string .= $line[0];
-      $insert_string .= ',' . $db->quote($line[1]);
+
+      $insert_string .= ',' . $db->quote(strtr(utf8_encode($line[1]), $cp1252_map));
       $insert_string .= ',' . 2; // France
       $insert_string .= ',' . $line[2];
       $insert_string .= ',' . $line[3];
       $insert_string .= ',' . $line[4];
       $insert_string .= ',' . $db->quote($items[0]);
-      $insert_string .= ',' . $db->quote($line[6]); // Location details
+      $insert_string .= ',' . $db->quote(strtr(utf8_encode($line[6]), $cp1252_map)); // Location details
       $insert_string .= ',' . $db->quote($line[7]); // Local amenities
       $insert_string .= ',' . $db->quote($line[8]); // Getting there
       $insert_string .= ',' . $db->quote($airport[0]); // Airport - sql this as per city
       $insert_string .= ',' . $db->quote($line[10]); // Location type
-      $insert_string .= ',' . $line[11]; // Latitude
+      $insert_string .= ',' . $db->quote($line[11]); // Latitude
       $insert_string .= ',' . $db->quote($line[12]); // Longitude
       $insert_string .= ',' . $db->quote($line[13]); // Distance to coast
       $insert_string .= ',' . $db->quote($line[14]); // video url
@@ -176,10 +212,10 @@ class ImportControllerProperty_listings extends JControllerForm {
       $insert_string .= ',' . $db->quote($line[23]); // tandc
       $insert_string .= ',' . $db->quote($line[24]); // use invoice details
       $insert_string .= ',' . $db->quote($line[25]);
-      $insert_string .= ',' . $db->quote($line[26]);
-      $insert_string .= ',' . $db->quote($line[27]);
-      $insert_string .= ',' . $db->quote($line[28]);
-      $insert_string .= ',' . $db->quote($line[29]);
+      $insert_string .= ',' . $db->quote(strtr(utf8_encode($line[26]), $cp1252_map));
+      $insert_string .= ',' . $db->quote(strtr(utf8_encode($line[27]), $cp1252_map));
+      $insert_string .= ',' . $db->quote(strtr(utf8_encode($line[28]), $cp1252_map));
+      $insert_string .= ',' . $db->quote(strtr(utf8_encode($line[29]), $cp1252_map));
       $insert_string .= ',' . $db->quote($line[30]);
       $insert_string .= ',' . $db->quote($line[31]);
       $insert_string .= ',' . $db->quote($line[32]);
@@ -203,7 +239,6 @@ class ImportControllerProperty_listings extends JControllerForm {
         die;
       }
     }
-
 
     fclose($handle);
 
