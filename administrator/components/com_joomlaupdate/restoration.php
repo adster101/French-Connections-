0bf51ebeb9fb589bc95a5d681d501436dea77262
Adam Rifat <adam@littledonkey.net>
2014-04-07 17:26:29 +0100

    Update to 3.2.3

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,13 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => 'NEUC28g6DX72c0XgvWN8EELnCwOJd8ed',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => 'C:\xampp\htdocs\tmp/Joomla_3.2.2_to_3.2.3-Stable-Patch_Package.zip',
+	'kickstart.setup.destdir' => 'C:\xampp\htdocs',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0');
