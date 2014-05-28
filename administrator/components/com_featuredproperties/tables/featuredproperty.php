5fd71aa212c6bac8f10ead99862f990e76cb1645
Adam Rifat <adam@littledonkey.net>
2014-05-28 10:52:56 +0100

    Added check to FP table class to ensure no more than four FP can be added for a specific date.

--- administrator/components/com_featuredproperties/tables/featuredproperty.php
+++ administrator/components/com_featuredproperties/tables/featuredproperty.php
@@ -1,4 +1,5 @@
 <?php
+
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
@@ -14,16 +15,42 @@ jimport('joomla.application.component.model');
 class FeaturedPropertiesTableFeaturedProperty extends JTable
 {
 
-	/**
-	 * Constructor
-	 *
-	 * @param object Database connector object
-	 */
-	function __construct(&$db)
-	{
-		parent::__construct('#__featured_properties', 'id', $db);
-	}
-  
-  
+  /**
+   * Constructor
+   *
+   * @param object Database connector object
+   */
+  function __construct(&$db)
+  {
+    parent::__construct('#__featured_properties', 'id', $db);
+  }
+
+  /**
+   * Basic check to ensure that there are no more than four FP for a given date.
+   * 
+   */
+  public function check()
+  {
+    // Check for existing name
+    $query = $this->_db->getQuery(true)
+            ->select('count(*) as count')
+            ->from($this->_db->quoteName('#__featured_properties'))
+            ->where($this->_db->quoteName('start_date') . ' = ' . $this->_db->quote($this->start_date))
+            ->where($this->_db->quoteName('featured_property_type') . ' = ' . $this->_db->quote($this->featured_property_type));
+    $this->_db->setQuery($query);
+    
+    $result = $this->_db->loadObject();
+
+    $count = (int) $result->count;
+    
+    if ($count >= 4)
+    {
+      $this->setError(JText::_('COM_FEATUREDPROPERTIES_MORE_THAN_FOUR'));
+      return false;
+    }
+      
+    return true;
+
+  }
 
 }
