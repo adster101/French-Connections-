b8acf80498c62dffd0f894c8bb84481d011c64f5
Adam Rifat <adam@littledonkey.net>
2014-10-13 14:55:52 +0100

    Fix for 521

--- components/com_accommodation/views/listing/tmpl/default_form.php
+++ components/com_accommodation/views/listing/tmpl/default_form.php
@@ -53,16 +53,68 @@ $errors = $app->getUserState('com_accommodation.enquiry.messages');
     <?php echo JHtml::_('form.token'); ?>
 
     <fieldset class="adminform">
-      <?php foreach ($this->form->getFieldset('enquiry') as $field): ?>
-        <div class="form-group">
-          <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
-          <?php echo $field->label; ?>
-            <?php echo $field->input; ?>
+      <div class="form-group">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+          <?php echo $this->form->getLabel('guest_forename'); ?>
+          <?php echo $this->form->getInput('guest_forename'); ?>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+          <?php echo $this->form->getLabel('guest_surname'); ?>
+          <?php echo $this->form->getInput('guest_surname'); ?>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+          <?php echo $this->form->getLabel('guest_email'); ?>
+          <?php echo $this->form->getInput('guest_email'); ?>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+          <?php echo $this->form->getLabel('guest_phone'); ?>
+          <?php echo $this->form->getInput('guest_phone'); ?>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+          <?php echo $this->form->getLabel('message'); ?>
+          <?php echo $this->form->getInput('message'); ?>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+          <?php echo $this->form->getLabel('start_date'); ?>  
+          <div class="input-group start_date date">
+            <?php echo $this->form->getInput('start_date'); ?> 
+            <span class="input-group-addon"><i class="glyphicon glyphicon-calendar" for="arrival"></i></span>
           </div>
-        </div>         
-      <?php endforeach; ?> 
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+          <?php echo $this->form->getLabel('end_date'); ?>  
+          <div class="input-group start_date date">
+            <?php echo $this->form->getInput('end_date'); ?> 
+            <span class="input-group-addon"><i class="glyphicon glyphicon-calendar" for="arrival"></i></span>
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+          <?php echo $this->form->getLabel('adults'); ?>
+          <?php echo $this->form->getInput('adults'); ?>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+          <?php echo $this->form->getLabel('children'); ?>
+          <?php echo $this->form->getInput('children'); ?>
+        </div>
+      </div>
     </fieldset>
-    
+
     <div class="form-actions"><button class="btn btn-primary btn-large validate" type="submit"><?php echo JText::_('COM_ACCOMMODATION_SEND_ENQUIRY'); ?></button>
       <input type="hidden" name="option" value="com_accommodation" />
       <input type="hidden" name="task" value="listing.enquiry" />
