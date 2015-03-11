72893f5511f909acf64542396c528f834c6ed05a
Adam Rifat <adam@littledonkey.net>
2015-03-11 14:05:12 +0000

    Removed class=row from carousel featured property view

--- modules/mod_featuredproperty/tmpl/carousel-search.php
+++ modules/mod_featuredproperty/tmpl/carousel-search.php
@@ -27,44 +27,42 @@ $chunks = array_chunk($items, 4);
     <div class="carousel-inner">   
       <?php foreach ($chunks as $key => $chunk) : ?>
         <div class="item <?php echo ($key == 0) ? 'active' : ''; ?>">   
-          <div class="row">
-            <?php foreach ($chunk as $item) : ?>
-              <?php
-              $prices = JHtml::_('general.price', $item->price, $item->base_currency, '', '');
-              $description = JHTml::_('string.truncate', $item->description, 75, true, false);
-              $title = JText::sprintf('MOD_FEATURED_PROPERTY_THUMB_TITLE', $item->id, $description);
-              $region = JRoute::_('index.php?option=com_fcsearch&s_kwds=' . $item->alias . '&lang=' . $lang . '&Itemid=' . (int) $Itemid_search);
-              $property = JRoute::_('index.php?option=com_accommodation&Itemid=' . (int) $Itemid_property . '&id=' . (int) $item->id . '&unit_id=' . (int) $item->unit_id);
-              ?>
-              <?php if ($item->title) : ?>        
-                <div class="col-lg-3 col-sm-3"> 
-                  <p>
-                    <a title ="<?php echo htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?>" class="" href="<?php echo $property ?>">
-                      <?php if (!empty($item->offer)) : ?>
-                        <span class="offer small" data-toggle="tooltip" title="<?php echo htmlspecialchars($item->offer) ?>">
-                          <?php echo JHtml::_('string.truncate', htmlspecialchars($item->offer), 35, true, false); ?>
-                        </span>
-                      <?php endif; ?>
-                      <img src='/images/property/<?php echo $item->unit_id . '/thumb/' . $item->thumbnail ?>' class="fp-media-object img-responsive" />
-                    </a>
-                  </p>
-                  <h4 class="fp-media-heading">
-                    <a href="<?php echo $region ?>"><?php echo htmlspecialchars($item->title); ?></a>
-                  </h4>
-                  <p>
-                    <?php echo JText::sprintf('MOD_FEATURED_PROPERTY_SLEEPS', $item->occupancy); ?>
-                    <?php if (!empty($item->price)) : ?> 
-                      <?php echo JText::sprintf('MOD_FEATURED_PROPERTY_PRICE_FROM', $prices['GBP']) ?>
-                    <?php endif; ?><br />
-                    <?php // echo JHTml::_('string.truncate', $item->description, 25, true, false); ?>
-                    <a title ="<?php echo htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?>" class="fp-thumbnail" href="<?php echo $property ?>">
-                      <?php echo htmlspecialchars($item->unit_title); ?>&nbsp;&raquo;
-                    </a>
-                  </p>     
-                </div>
-              <?php endif; ?>
-            <?php endforeach; ?>
-          </div>
+          <?php foreach ($chunk as $item) : ?>
+            <?php
+            $prices = JHtml::_('general.price', $item->price, $item->base_currency, '', '');
+            $description = JHTml::_('string.truncate', $item->description, 75, true, false);
+            $title = JText::sprintf('MOD_FEATURED_PROPERTY_THUMB_TITLE', $item->id, $description);
+            $region = JRoute::_('index.php?option=com_fcsearch&s_kwds=' . $item->alias . '&lang=' . $lang . '&Itemid=' . (int) $Itemid_search);
+            $property = JRoute::_('index.php?option=com_accommodation&Itemid=' . (int) $Itemid_property . '&id=' . (int) $item->id . '&unit_id=' . (int) $item->unit_id);
+            ?>
+            <?php if ($item->title) : ?>        
+              <div class="col-lg-3 col-sm-3"> 
+                <p>
+                  <a title ="<?php echo htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?>" class="" href="<?php echo $property ?>">
+                    <?php if (!empty($item->offer)) : ?>
+                      <span class="offer small" data-toggle="tooltip" title="<?php echo htmlspecialchars($item->offer) ?>">
+                        <?php echo JHtml::_('string.truncate', htmlspecialchars($item->offer), 35, true, false); ?>
+                      </span>
+                    <?php endif; ?>
+                    <img src='/images/property/<?php echo $item->unit_id . '/thumb/' . $item->thumbnail ?>' class="fp-media-object img-responsive" />
+                  </a>
+                </p>
+                <h4 class="fp-media-heading">
+                  <a href="<?php echo $region ?>"><?php echo htmlspecialchars($item->title); ?></a>
+                </h4>
+                <p>
+                  <?php echo JText::sprintf('MOD_FEATURED_PROPERTY_SLEEPS', $item->occupancy); ?>
+                  <?php if (!empty($item->price)) : ?> 
+                    <?php echo JText::sprintf('MOD_FEATURED_PROPERTY_PRICE_FROM', $prices['GBP']) ?>
+                  <?php endif; ?><br />
+                  <?php // echo JHTml::_('string.truncate', $item->description, 25, true, false); ?>
+                  <a title ="<?php echo htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?>" class="fp-thumbnail" href="<?php echo $property ?>">
+                    <?php echo htmlspecialchars($item->unit_title); ?>&nbsp;&raquo;
+                  </a>
+                </p>     
+              </div>
+            <?php endif; ?>
+          <?php endforeach; ?>
         </div>
       <?php endforeach; ?>  
     </div>   
