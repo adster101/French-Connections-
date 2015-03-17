e32a5a03aa2f082767905913432070a3c0b80f8f
Adam Rifat <adam@littledonkey.net>
2015-03-17 17:12:32 +0000

    Added new contact form component

---
+++ components/com_fccontact/models/contact.php
@@ -0,0 +1,139 @@
+<?php
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.error.log');
+
+/**
+ * HelloWorld Model
+ */
+class FcContactModelContact extends JModelAdmin
+{
+
+  /**
+   * @var object item
+   */
+  protected $item;
+
+  /**
+   * Method to get the contact form.
+   *
+   * The base form is loaded from XML and then an event is fired
+   *
+   *
+   * @param	array	$data		An optional array of data for the form to interrogate.
+   * @param	boolean	$loadData	True if the form is to load its own data (default case), false if not.
+   * @return	JForm	A JForm object on success, false on failure
+   * @since	1.6
+   */
+  public function getForm($data = array(), $loadData = false)
+  {
+    // Get the form.
+    $form = $this->loadForm('com_fccontact.contact', 'contact', array('control' => 'jform', 'load_data' => true));
+    if (empty($form))
+    {
+      return false;
+    }
+
+
+    return $form;
+  }
+
+  /**
+   * Method to get the data that should be injected in the form.
+   *
+   * @return	mixed	The data for the form.
+   * @since	1.6
+   */
+  protected function loadFormData()
+  {
+    // Check the session for previously entered form data.
+    $data = JFactory::getApplication()->getUserState('com_fccontact.contact.data', array());
+
+    if (empty($data))
+    {
+      $data = array();
+    }
+
+    return $data;
+  }
+
+  public function preprocessForm(JForm $form, $data, $group = 'content')
+  {
+    parent::preprocessForm($form, $data, $group);
+
+    $input = JFactory::getApplication()->input;
+
+    $presales = $input->get('pre-sales', false, 'boolean');
+
+    if ($presales)
+    {
+      $form->setFieldAttribute('nature', 'default', 'COM_FCCONTACT_NATURE_OF_ENQUIRY_PRE_SALES');
+    }
+  }
+
+  /**
+   * Method to auto-populate the model state.
+   *
+   * This method should only be called once per instantiation and is designed
+   * to be called on the first call to the getState() method unless the model
+   * configuration flag to ignore the request is set.
+   *
+   * Note. Calling getState in this method will result in recursion.
+   *
+   * @return	void
+   * @since	1.6
+   */
+  protected function populateState()
+  {
+    
+  }
+
+  /**
+   * 
+   * @param type $data
+   * @return boolean
+   */
+  public function save($data)
+  {
+
+    $app = JFactory::getApplication();
+    $menuItem = $app->getMenu()->getActive();
+    $params = $menuItem->params;
+
+    $subject = JText::sprintf(
+                    'COM_FCCONTACT_EMAIL_SUBJECT', JText::_($data['nature']), $data['prn']
+    );
+
+    $body = JText::sprintf(
+                    'COM_FCCONTACT_EMAIL_BODY', $data['message'], $data['tel']
+    );
+
+    $from = $data['email'];
+
+    $name = $data['name'];
+
+    $to = $params->get('contact', 'fchelpdesk@frenchconnections.co.uk');
+    
+    if ($data['nature'] == 'COM_FCCONTACT_NATURE_OF_ENQUIRY_PRE_SALES')
+    {
+      $cc = $params->get('additional_email');
+    }
+    
+    // Send the registration email. the true argument means it will go as HTML
+    $send = JFactory::getMailer()
+            ->sendMail($from, $name, $to, $subject, $body, true, $cc);
+
+    if (!$send)
+    {
+      // Log out to file that email wasn't sent for what ever reason;
+      // Trigger email to admin / office user. e.g. as per registration.php
+      Throw new Exception('Problem sending email for contact form.');
+    }
+
+    // Return the user object we've just created
+    return true;
+  }
+
+}
---
+++ components/com_fccontact/models/fields/diallingcodes.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  User.profile
+ *
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+defined('JPATH_PLATFORM') or die;
+
+JFormHelper::loadFieldClass('list');
+
+/**
+ * Provides input for TOS
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  User.profile
+ * @since       2.5.5
+ */
+class JFormFieldDiallingcodes extends JFormFieldList {
+
+  /**
+   * The form field type.
+   *
+   * @var    string
+   * @since  2.5.5
+   */
+  public $type = 'Diallingcodes';
+
+
+  /**
+   * Method to get the field options.
+   *
+   * @return  array  The field option objects.
+   *
+   * @since   11.1
+   */
+  protected function getOptions() {
+    $options = array();
+    // Initialize variables.
+    
+    $options = array();
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+
+    $query->select('a.lang_string, dialling_code');
+    $query->from('#__countries AS a');
+    $query->order('a.id asc');
+
+
+    $db->setQuery($query);
+    $items = $db->loadObjectList();
+
+    $options = array();
+
+    // Check for a database error.
+    if ($db->getErrorNum()) {
+      JError::raiseWarning(500, $db->getErrorMsg());
+    }
+    // Loop over each subtree item
+    foreach ($items as &$item) {
+      $options[] = JHtml::_('select.option', $item->dialling_code, JText::_($item->lang_string) . ' ' . $item->dialling_code );
+    }
+
+    // Merge any additional options in the XML definition.
+    $options = array_merge(parent::getOptions(), $options);
+    return $options;
+  }
+
+}
