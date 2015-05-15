ad72214c51ece716977e8e850a3e5e7ddbfa3845
Adam Rifat <adam@littledonkey.net>
2015-05-15 14:30:17 +0100

    Updated car trawler booking engine

--- modules/mod_car_trawler/mod_car_trawler.php
+++ modules/mod_car_trawler/mod_car_trawler.php
@@ -10,28 +10,7 @@
 defined('_JEXEC') or die;
 
 $doc = JFactory::getDocument();
-$uri = JUri::getInstance();
 
-//	looking for scripts
-$headers = $doc->getHeadData();
-$scripts = isset($headers['scripts']) ? $headers['scripts'] : array();
-//	cleare the original scripts
-$headers['scripts'] = array();
-$headers['script'] = array();
+$doc->addScript('//ajaxgeo.cartrawler.com/abe4.0/ct_loader.js?' . time(), 'text/javascript', false, true);
 
-//	This removes all scripts from the head, conflicts with the booking engine ensue otherwise
-foreach ($scripts as $url => $type) {
-
-  $relativePath = trim(str_replace($uri->getPath(), '', JUri::root()), '/');
-  $relativeScript = trim(str_replace($uri->getPath(), '', $url), '/');
-  $relativeUrl = str_replace($relativePath, '', $url);
-
-  // Try to disable relative and full URLs
-  unset($doc->_scripts[$url]);
-  unset($doc->_scripts[$relativeUrl]);
-  unset($doc->_scripts[JUri::root(true) . $url]);
-  unset($doc->_scripts[$relativeScript]);
-}
-
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 require JModuleHelper::getLayoutPath('mod_car_trawler', $params->get('layout', 'default'));
