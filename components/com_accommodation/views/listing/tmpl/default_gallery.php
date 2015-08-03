f87af87e2da7de81ac641b70a232dc7564f144f3
Adam Rifat <adam@littledonkey.net>
2015-08-03 13:53:27 +0100

    Added CDN parameter to rental accommodation compo

--- components/com_accommodation/views/listing/tmpl/default_gallery.php
+++ components/com_accommodation/views/listing/tmpl/default_gallery.php
@@ -1,5 +1,9 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
+$app = JFactory::getApplication();
+$menu = $app->getMenu()->getActive();
+$cdn = $menu->params->get('cdn', '');
+
 ?>
 
 <!-- Image gallery -->
@@ -19,12 +23,12 @@ defined('_JEXEC') or die('Restricted access');
         $src = (!empty($image->image_file_name)) ? JURI::root() . 'images/property/' . $this->item->unit_id . '/profiles/' . $image->image_file_name : JURI::getInstance()->toString(array('scheme')) . $image->url; ?>
         <?php
         $srcset = array();
-        $srcset[] = (!empty($image->image_file_name)) ? 'http://4e2a14ede1b074f03957-a358347f37218a98b98454e9b3f3a83e.r94.cf3.rackcdn.com/' . $this->item->unit_id . '/210x120_' . $image->image_file_name . $image->url . ' 210w' : JURI::getInstance()->toString(array('scheme')) . $image->url . ' 210w';
-        $srcset[] = (!empty($image->image_file_name)) ? 'http://4e2a14ede1b074f03957-a358347f37218a98b98454e9b3f3a83e.r94.cf3.rackcdn.com/' . $this->item->unit_id . '/330x248_' . $image->image_file_name . $image->url . ' 330w' : JURI::getInstance()->toString(array('scheme')) . $image->url . ' 330w';
-        $srcset[] = (!empty($image->image_file_name)) ? 'http://4e2a14ede1b074f03957-a358347f37218a98b98454e9b3f3a83e.r94.cf3.rackcdn.com/' . $this->item->unit_id . '/408x307_' . $image->image_file_name . $image->url . ' 408w' : JURI::getInstance()->toString(array('scheme')) . $image->url . ' 408w';
-        $srcset[] = (!empty($image->image_file_name)) ? 'http://4e2a14ede1b074f03957-a358347f37218a98b98454e9b3f3a83e.r94.cf3.rackcdn.com/' . $this->item->unit_id . '/617x464_' . $image->image_file_name . $image->url . ' 617w' : JURI::getInstance()->toString(array('scheme')) . $image->url . ' 617w';
-        $srcset[] = (!empty($image->image_file_name)) ? 'http://4e2a14ede1b074f03957-a358347f37218a98b98454e9b3f3a83e.r94.cf3.rackcdn.com/' . $this->item->unit_id . '/770x580_' . $image->image_file_name . $image->url . ' 770w' : JURI::getInstance()->toString(array('scheme')) . $image->url. ' 770w';
-        $srcset[] = (!empty($image->image_file_name)) ? 'http://4e2a14ede1b074f03957-a358347f37218a98b98454e9b3f3a83e.r94.cf3.rackcdn.com/' . $this->item->unit_id . '/903x586_' . $image->image_file_name . $image->url . ' 903w' : JURI::getInstance()->toString(array('scheme')) . $image->url. ' 903w';
+        $srcset[] = (!empty($image->image_file_name)) ? $cdn . $this->item->unit_id . '/210x120_' . $image->image_file_name . $image->url . ' 210w' : JURI::getInstance()->toString(array('scheme')) . $image->url . ' 210w';
+        $srcset[] = (!empty($image->image_file_name)) ? $cdn . $this->item->unit_id . '/330x248_' . $image->image_file_name . $image->url . ' 330w' : JURI::getInstance()->toString(array('scheme')) . $image->url . ' 330w';
+        $srcset[] = (!empty($image->image_file_name)) ? $cdn . $this->item->unit_id . '/408x307_' . $image->image_file_name . $image->url . ' 408w' : JURI::getInstance()->toString(array('scheme')) . $image->url . ' 408w';
+        $srcset[] = (!empty($image->image_file_name)) ? $cdn . $this->item->unit_id . '/617x464_' . $image->image_file_name . $image->url . ' 617w' : JURI::getInstance()->toString(array('scheme')) . $image->url . ' 617w';
+        $srcset[] = (!empty($image->image_file_name)) ? $cdn . $this->item->unit_id . '/770x580_' . $image->image_file_name . $image->url . ' 770w' : JURI::getInstance()->toString(array('scheme')) . $image->url. ' 770w';
+        $srcset[] = (!empty($image->image_file_name)) ? $cdn . $this->item->unit_id . '/903x586_' . $image->image_file_name . $image->url . ' 903w' : JURI::getInstance()->toString(array('scheme')) . $image->url. ' 903w';
         ?>
         <div>
           <?php if ($images == 0) : ?>
@@ -50,7 +54,7 @@ defined('_JEXEC') or die('Restricted access');
         </div>
       <?php endif; ?>
       <?php foreach ($this->images as $images => $image) : ?> 
-        <?php $src = (!empty($image->image_file_name)) ? 'http://4e2a14ede1b074f03957-a358347f37218a98b98454e9b3f3a83e.r94.cf3.rackcdn.com/' . $this->item->unit_id . '/100x100_' . $image->image_file_name : JURI::getInstance()->toString(array('scheme')) . $image->url_thumb; ?>
+        <?php $src = (!empty($image->image_file_name)) ? $cdn . $this->item->unit_id . '/100x100_' . $image->image_file_name : JURI::getInstance()->toString(array('scheme')) . $image->url_thumb; ?>
         <div>
           <img src="<?php echo $src ?>" /> 
         </div>     
