43afe481eb882966fd0a113bac8623064cfc7c90
Adam Rifat <adam@littledonkey.net>
2015-08-17 11:09:34 +0100

    Corrected assets.tmp.php for admin scripts ready for 2.6 release

--- administrator/templates/fcadmin/assets.php
+++ administrator/templates/fcadmin/assets.php
@@ -3,21 +3,20 @@
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
-$URI = JURI::getInstance();
 $doc = JFactory::getDocument();
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
+
 if ($view == 'images' && ($option == 'com_rental' || $option == 'com_realestate'))
 {
-  $script_path = (JDEBUG) ? '/media/fc/assets/js/images.admin.scripts.js' : '/media/fc/assets/js/2015072316826.images.admin.scripts.min.js';
+  $script_path = (JDEBUG) ? '/media/fc/assets/js/images.admin.scripts.js' : '/media/fc/assets/js/2015081711231.images.admin.scripts.min.js';
 }
 else
 {
-  $script_path = (JDEBUG) ? '/media/fc/assets/js/admin.scripts.js' : '/media/fc/assets/js/2015072316826.admin.scripts.min.js';
+  $script_path = (JDEBUG) ? '/media/fc/assets/js/admin.scripts.js' : '/media/fc/assets/js/2015081711231.admin.scripts.min.js';
 }
 
-//$css_path = (JDEBUG) ? '/media/fc/assets/css/admin.styles.css' : '/media/fc/assets/css/2015072316826.admin.styles.min.css';
 //$doc->addStyleSheet('//' . $URI->getHost() . $css_path);
-$doc->addScript('//' . $URI->getHost() . $script_path);
+$doc->addScript('//' . $uri->getHost() . $script_path);
 
