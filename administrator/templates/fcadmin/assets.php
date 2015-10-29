3e347d7f90ee8bef3f8ece780c6c94d51470c840
adster101 <adam@littledonkey.net>
2015-10-29 10:25:07 +0000

    Assets

--- administrator/templates/fcadmin/assets.php
+++ administrator/templates/fcadmin/assets.php
@@ -10,19 +10,11 @@ defined('_JEXEC') or die('Restricted access');
 
 if ($view == 'images' && ($option == 'com_rental' || $option == 'com_realestate'))
 {
-<<<<<<< HEAD
-  $script_path = (JDEBUG) ? '/media/fc/js/images.admin.scripts.js' : '/media/fc/assets/js/20151026165818.images.admin.scripts.min.js';
+  $script_path = (JDEBUG) ? '/media/fc/js/images.admin.scripts.js' : '/media/fc/assets/js/2015102994734.images.admin.scripts.min.js';
 }
 else
 {
-  $script_path = (JDEBUG) ? '/media/fc/js/admin.scripts.js' : '/media/fc/assets/js/20151026165818.admin.scripts.min.js';
-=======
-  $script_path = (JDEBUG) ? '/media/fc/js/images.admin.scripts.js' : '/media/fc/assets/js/20151028133258.images.admin.scripts.min.js';
-}
-else
-{
-  $script_path = (JDEBUG) ? '/media/fc/js/admin.scripts.js' : '/media/fc/assets/js/20151028133258.admin.scripts.min.js';
->>>>>>> release-2.8
+  $script_path = (JDEBUG) ? '/media/fc/js/admin.scripts.js' : '/media/fc/assets/js/2015102994734.admin.scripts.min.js';
 }
 
 //$doc->addStyleSheet('//' . $URI->getHost() . $css_path);
