fe98840bd1720ebd771007f759e0272f5a48125c
adster101 <adam@littledonkey.net>
2015-12-10 09:29:30 +0000

    Grunt'ed before merging to master

--- templates/fcv4/inc/assets.php
+++ templates/fcv4/inc/assets.php
@@ -7,8 +7,8 @@ $doc = JFactory::getDocument();
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
-$script_path = (JDEBUG) ? '/media/fc/js/scripts.js' : '/media/fc/assets/js/2015120295757.scripts.min.js';
-$css_path = (JDEBUG) ? '/media/fc/css/styles.css' : '/media/fc/assets/css/2015120295757.styles.min.css';
+$script_path = (JDEBUG) ? '/media/fc/js/scripts.js' : '/media/fc/assets/js/2015121092842.scripts.min.js';
+$css_path = (JDEBUG) ? '/media/fc/css/styles.css' : '/media/fc/assets/css/2015121092842.styles.min.css';
 
 //$doc->addStyleSheet('//' . $URI->getHost() . $css_path);
 $doc->addScript('//' . $URI->getHost() . $script_path, 'text/javascript', true, true);
