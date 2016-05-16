e5c9090b88043762f4468f3144cb3376cc4ebacc
adster101 <adam@littledonkey.net>
2016-05-16 11:22:40 +0100

    Grunt before merge to master

--- templates/fcv4/inc/styles.php
+++ templates/fcv4/inc/styles.php
@@ -1,7 +1,7 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
-$css_path = (JDEBUG) ? '/media/fc/css/styles.css' : '/media/fc/assets/css/20160405163525.styles.min.css';
+$css_path = (JDEBUG) ? '/media/fc/css/styles.css' : '/media/fc/assets/css/20160516112221.styles.min.css';
 ?>
 <script id="loadcss">
     (function (w) {
