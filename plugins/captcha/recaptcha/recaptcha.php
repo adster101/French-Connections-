7a497a955859cc8f14c5f4f31ae0ba6c8cc87d13
Adam Rifat <adam@littledonkey.net>
2017-03-28 14:14:57 +0100

    Updated to J-3.6.5

--- plugins/captcha/recaptcha/recaptcha.php
+++ plugins/captcha/recaptcha/recaptcha.php
@@ -60,7 +60,7 @@ class PlgCaptchaRecaptcha extends JPlugin
 		{
 			// Load callback first for browser compatibility
 			JHtml::_('script', 'plg_captcha_recaptcha/recaptcha.min.js', false, true);
-			
+
 			$file = 'https://www.google.com/recaptcha/api.js?onload=JoomlaInitReCaptcha2&render=explicit&hl=' . JFactory::getLanguage()->getTag();
 			JHtml::_('script', $file);
 		}
@@ -88,11 +88,11 @@ class PlgCaptchaRecaptcha extends JPlugin
 		}
 		else
 		{
-			return '<div id="' . $id . '" ' . str_replace('class="', 'class="g-recaptcha ', $class) .
-					' data-sitekey="' . $this->params->get('public_key', '') .
-					'" data-theme="' . $this->params->get('theme2', 'light') .
-					'" data-size="' . $this->params->get('size', 'normal') .
-					'"></div>';
+			return '<div id="' . $id . '" ' . str_replace('class="', 'class="g-recaptcha ', $class)
+					. ' data-sitekey="' . $this->params->get('public_key', '')
+					. '" data-theme="' . $this->params->get('theme2', 'light')
+					. '" data-size="' . $this->params->get('size', 'normal')
+					. '"></div>';
 		}
 	}
 
